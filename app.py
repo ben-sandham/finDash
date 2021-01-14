05bace925c12b9881c1d160c9d71b012efd0682e
Ben Sandham <bsandham977@gmail.com>
2021-01-13 23:22:33 -0500

    Add input and dropdown core components

--- app.py
+++ app.py
@@ -7,34 +7,51 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import yfinance as yf
 import pandas as pd
+
+from controls import PERIODS
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+tick = yf.Ticker('IPOF')
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df = tick.history(period='max')
+df = df.loc[:, 'Close']
+
+fig = px.line(df, x=df.index, y="Close", title="Closing Price Chart", labels={"Close":"Close Price (USD)"})
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Stock Chart Example'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    dcc.Input(
+        id="tick-input",
+        placeholder="input ticker symbol"
+    ),
+
+    dcc.Dropdown(
+        id="period-interval",
+        multi=True,
+        value=[interval for interval in PERIODS]
+    ),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
     )
 ])
+
+def create_close_price_df(ticker, period='1y'):
+    tick = yf.Ticker(ticker)
+    df = tick.history(period=period)
+    df = df.loc[:, "Close"]
+    return df
+
+# @app.callback(
+#     Output("")
+# )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ controls.py
@@ -0,0 +1,2 @@
+# Controls for webapp
+PERIODS = ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max']
